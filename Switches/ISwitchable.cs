4ca493f4136a5bc43ca71131fad6299510ae6257
Malick McGregor <mmcgregor@fsw.com>
2015-02-20 13:42:39 -0700

    Added State to ISwitchable interface.

--- Switches/ISwitchable.cs
+++ Switches/ISwitchable.cs
@@ -1,7 +1,15 @@
 ﻿namespace Switches
 {
+    public enum SwitchState
+    {
+        Off,
+        On
+    }
+
     public interface ISwitchable
     {
+        SwitchState State { get; }
+
         void Switch();
     }
 }
